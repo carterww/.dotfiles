89078debff779a0e2839f02c2a595cd0a54f6ad0
Carter Williams <carterww@hotmail.com>
2024-06-22 08:41:30 -0500

    update wm script

--- scripts/install-wm.sh
+++ scripts/install-wm.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
+REPO_PATH="$HOME/code/suckless"
+
 IMG="/usr/share/pixmaps/default-background.jpg"
 
-DWM_DIR="$HOME/code/dwm"
+DWM_DIR="$REPO_PATH/dwm"
 DWM_REPO="https://github.com/carterww/dwm.git"
 
-SLSTATUS_DIR="$HOME/code/slstatus"
+SLSTATUS_DIR="$REPO_PATH/slstatus"
 SLSTATUS_REPO="https://github.com/carterww/slstatus.git"
+
+SLOCK_DIR="$REPO_PATH/slock"
+SLOCK_REPO="https://github.com/carterww/slock.git"
 
 echo "Installing dependencies using pacman"
 # I think these are all but I could be missing some
 # This includes EVERYTHING dwm uses that is hardcoded
 pacman -Sy --noconfirm xorg-server xorg-apps libx11                 \
               ttf-roboto-mono-nerd ttf-font-awesome dmenu playerctl \
-              picom feh xss-lock firefox i3lock pulseaudio curl
+              picom feh xss-lock pulseaudio curl
+
+if test -d $REPO_PATH; then
+    echo "The suckless directory already exists in $REPO_PATH."
+else
+    mkdir -p $REPO_PATH
+fi
 
 # Clone my dwm and slstatus repos into my code directory
 if test -d $DWM_DIR; then
@@ -28,12 +39,19 @@ else
     git clone --depth=1 $SLSTATUS_REPO $SLSTATUS_DIR
 fi
 
+if test -d $SLOCK_DIR; then
+    echo "slock is already installed in $SLOCK_DIR."
+else
+    git clone --depth=1 $SLOCK_REPO $SLOCK_DIR
+fi
+
 echo "Compiling and installing dwm and slstatus."
 if [[ $1 == *"i"* ]]; then
     make -C $DWM_DIR clean install
     make -C $SLSTATUS_DIR clean install
+    make -C $SLOCK_DIR clean install
 else
-  echo "Not installing dwm or slstatus. Run 'make install' as root for each."
+  echo "Not installing dwm, slstatus, or slock. Run 'make install' as root for each."
   echo "REMINDER: Be sure to define LAPTOP_BUILD in config.h if it's for my laptop."
 fi
 
